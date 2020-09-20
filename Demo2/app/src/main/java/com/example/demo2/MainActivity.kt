b4252e8b1b28fb6d839070163aa2752e95b08556
peter <s940966116@gmail.com>
2020-09-20 17:04:04 +0800

    phone

--- Demo2/app/src/main/java/com/example/demo2/MainActivity.kt
+++ Demo2/app/src/main/java/com/example/demo2/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.demo2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_horizon.view.*
 
+data class Item(
+    val name: String,
+    val phone: String
+)
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: MyAdapter
+    private val contact = ArrayList<Item>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)0
+        setContentView(R.layout.activity_main)
+
+        val linearManager = LinearLayoutManager(this)
+        linearManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = linearManager
+        adapter = MyAdapter(contact)
+        recyclerView.adapter = adapter
+
+        button1.setOnClickListener {
+            startActivityForResult(Intent(this, MainActivity2::class.java),1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.extras?.let {
+            if(requestCode==1 && resultCode==Activity.RESULT_OK){
+                contact.add(Item(it.getString("name").toString(),it.getString("phone").toString()))
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+}
+
+class MyAdapter(private val mData:ArrayList<Item>): RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+    class ViewHolder(v: View): RecyclerView.ViewHolder(v){
+        val name = v.textView3
+        val phone = v.textView4
+        val delete = v.imageView2
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_horizon, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = mData[position].name
+        holder.phone.text = mData[position].phone
+        holder.delete.setOnClickListener {
+            mData.removeAt(position)
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mData.size
     }
 }
--- Demo2/app/src/main/java/com/example/demo2/MainActivity2.kt
+++ Demo2/app/src/main/java/com/example/demo2/MainActivity2.kt
@@ -1,14 +1,30 @@
 package com.example.demo2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
 
-data class Item{
-    val name: S
-}
+
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        button.setOnClickListener {
+            when{
+                input1.length()<1 -> Toast.makeText(this,"please input name",Toast.LENGTH_SHORT).show()
+                input2.length()<1 -> Toast.makeText(this,"please input phone number", Toast.LENGTH_SHORT).show()
+                else -> {
+                    val b = Bundle()
+                    b.putString("name",input1.text.toString())
+                    b.putString("phone", input2.text.toString())
+                    setResult(Activity.RESULT_OK, Intent().putExtras(b))
+                    finish()
+                }
+            }
+        }
     }
 }
